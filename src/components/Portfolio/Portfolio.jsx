d5719e9f1fd1466a31c29880b095dc4778bbc109
Valentina Lugovaia <vaal.it@yandex.ru>
2023-04-17 16:02:00 +0300

    fixed links

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,16 +7,16 @@ function Portfolio() {
             <h3 className="portfolio__title">Портфолио</h3>
             <ul className="portfolio__list">
                 <li className="portfolio__item">
-                    <a className="portfolio__link" href="https://github.com/VaalIT/how-to-learn" target="_blank" rel="noreferrer">Статичный сайт</a>
-                    <a className="portfolio__link" href="https://github.com/VaalIT/how-to-learn" target="_blank" rel="noreferrer"><img className="portfolio__button" src={image} alt="Стрелка." /></a>
+                    <a className="portfolio__link" href="https://github.com/ValLugovaia/how-to-learn" target="_blank" rel="noreferrer">Статичный сайт</a>
+                    <a className="portfolio__link" href="https://github.com/ValLugovaia/how-to-learn" target="_blank" rel="noreferrer"><img className="portfolio__button" src={image} alt="Стрелка." /></a>
                 </li>
                 <li className="portfolio__item">
-                    <a className="portfolio__link" href="https://github.com/VaalIT/russian-travel" target="_blank" rel="noreferrer">Адаптивный сайт</a>
-                    <a className="portfolio__link" href="https://github.com/VaalIT/russian-travel" target="_blank" rel="noreferrer"><img className="portfolio__button" src={image} alt="Стрелка." /></a>
+                    <a className="portfolio__link" href="https://github.com/ValLugovaia/russian-travel" target="_blank" rel="noreferrer">Адаптивный сайт</a>
+                    <a className="portfolio__link" href="https://github.com/ValLugovaia/russian-travel" target="_blank" rel="noreferrer"><img className="portfolio__button" src={image} alt="Стрелка." /></a>
                 </li>
                 <li className="portfolio__item">
-                    <a className="portfolio__link" href="https://github.com/VaalIT/react-mesto-api-full" target="_blank" rel="noreferrer">Одностраничное приложение</a>
-                    <a className="portfolio__link" href="https://github.com/VaalIT/react-mesto-api-full" target="_blank" rel="noreferrer"><img className="portfolio__button" src={image} alt="Стрелка." /></a>
+                    <a className="portfolio__link" href="https://github.com/ValLugovaia/react-mesto-api-full" target="_blank" rel="noreferrer">Одностраничное приложение</a>
+                    <a className="portfolio__link" href="https://github.com/ValLugovaia/react-mesto-api-full" target="_blank" rel="noreferrer"><img className="portfolio__button" src={image} alt="Стрелка." /></a>
                 </li>
             </ul>
         </section>
